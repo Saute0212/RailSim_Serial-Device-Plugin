b8f81079f5a676d2bff4bc0d87d606f06ada67bd
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-05-30 00:34:11 +0900

    "SerialDevicePlugin.cs"にメソッドを追加

--- SerialDevicePlugin/FormConfig.Designer.cs
+++ SerialDevicePlugin/FormConfig.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormConfig";
+            this.Name = "FormConfig";
+            this.Text = "Serial Device Plugin Settings -v1.0.0";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SerialDevicePlugin/SerialDevicePlugin.cs
+++ SerialDevicePlugin/SerialDevicePlugin.cs
@@ -1,11 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Mackoy.Bvets;
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
 
 namespace SerialDevicePlugin
 {
-    public class SerialDevicePlugin
+    //"Mackoy.Bvets.IInputDevice"のクラスを継承
+    public class SerialDevicePlugin : Mackoy.Bvets.IInputDevice
     {
+        public event InputEventHandler KeyDown; //キーが押されたときのイベント
+        public event InputEventHandler KeyUp; //キーを離したときのイベント
+        public event InputEventHandler LeverMoved; //ハンドルを操作したときのイベント
+
+        //プラグインが読み込まれたときに実行
+        public void Load(string SettingsXmlPath)
+        {
+
+        }
+
+        //プラグイン・シナリオが読み込まれたときに実行
+        public void SetAxisRanges(int[][] ranges)
+        {
+
+        }
+
+        //毎フレーム実行
+        public void Tick()
+        {
+
+        }
+
+        //プラグインの設定画面を表示
+        public void Configure(IWin32Window owner)
+        {
+
+        }
+
+        //BVEの終了時に実行
+        public void Dispose()
+        {
+
+        }
+
+        //COMポートを開く
+        private void openPort()
+        {
+
+        }
+
+        //COMポートを閉じる
+        private void closePort()
+        {
+
+        }
+
+        //ハンドルを操作したときに実行：LeverMovedイベント
+        private void onLeverMoved(int axis, int notch)
+        {
+
+        }
+
+        //キーを押したときに実行：KeyDownイベント
+        private void onKeyDown(int axis, int keyCode)
+        {
+
+        }
+
+        //キーを離したときに実行：KeyUpイベント
+        private void onKeyUp(int axis, int keyCode)
+        {
+
+        }
     }
 }
