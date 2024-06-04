fa43791129aae45de80239b07e2ecd4dd90eb882
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-06-05 02:44:31 +0900

    機能追加：ボタンの設定・フォーム読み込み時に実行する関数の指定

--- SerialDevicePlugin/FormConfig.cs
+++ SerialDevicePlugin/FormConfig.cs
@@ -12,6 +12,11 @@ namespace SerialDevicePlugin
 {
     public partial class FormConfig : Form
     {
+        string[] ComSpeed = { "300", "600", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200" };
+        private string[] cabSwitchKeyTexts = { "Horn 1", "Horn 2", "Constant Speed Control", "Music Horn" };
+        private string[] atsKeyTexts = new string[16];
+        private List<int[]> keyCodeTable = new List<int[]>();
+
         public FormConfig()
         {
             InitializeComponent();
@@ -19,9 +24,8 @@ namespace SerialDevicePlugin
             MinimumSize = new Size(455, 505);
             MaximumSize = new Size(455, 505);
 
-            string[] ComSpeed = {"300", "600", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200"};
-            ComSpeedList.Items.AddRange( ComSpeed );
-            label2.Text = "Failed";
+            //読み込み時に実行する関数
+            SetUp();
             ReadingComPorts();
         }
 
@@ -33,6 +37,107 @@ namespace SerialDevicePlugin
             foreach (string port in ports)
             {
                 ComPorts.Items.Add(port);
+            }
+        }
+
+        //Formの初期設定関数
+        private void SetUp()
+        {
+            //ComboBox・Label初期設定
+            for(int i = 0; i < atsKeyTexts.Length; i++)
+            {
+                atsKeyTexts[i] = "ATS" + i.ToString();
+            }
+            ComSpeedList.Items.AddRange(ComSpeed);
+            label2.Text = "Failed";
+
+            //初期設定
+            for(int i = 0; i < cabSwitchKeyTexts.Length; i++)
+            {
+                AddKey(cabSwitchKeyTexts[i]);
+                keyCodeTable.Add(new int[2] { -1, i });
+            }
+
+            for(int i = 0; i < atsKeyTexts.Length; i++)
+            {
+                AddKey(atsKeyTexts[i]);
+                keyCodeTable.Add(new int[2] { -2, i });
+            }
+
+            if(SerialDevicePlugin.settings.Port >=1 && SerialDevicePlugin.settings.Port <= 256)
+            {
+                ComPorts.SelectedIndex = SerialDevicePlugin.settings.Port - 1;
+            }
+
+            if(SerialDevicePlugin.settings.Speed != 0)
+            {
+                ComSpeedList.SelectedIndex = SerialDevicePlugin.settings.Speed -1;
+            }
+
+            SelectDropDownList(Btn1List, SerialDevicePlugin.settings.Button1);
+            SelectDropDownList(Btn2List, SerialDevicePlugin.settings.Button2);
+            SelectDropDownList(Btn3List, SerialDevicePlugin.settings.Button3);
+            SelectDropDownList(Btn4List, SerialDevicePlugin.settings.Button4);
+            SelectDropDownList(Btn5List, SerialDevicePlugin.settings.Button5);
+            SelectDropDownList(Btn6List, SerialDevicePlugin.settings.Button6);
+            SelectDropDownList(Btn7List, SerialDevicePlugin.settings.Button7);
+            SelectDropDownList(Btn8List, SerialDevicePlugin.settings.Button8);
+            SelectDropDownList(Btn9List, SerialDevicePlugin.settings.Button9);
+            SelectDropDownList(Btn10List, SerialDevicePlugin.settings.Button10);
+            SelectDropDownList(Btn11List, SerialDevicePlugin.settings.Button11);
+            SelectDropDownList(Btn12List, SerialDevicePlugin.settings.Button12);
+            SelectDropDownList(Btn13List, SerialDevicePlugin.settings.Button13);
+            SelectDropDownList(Btn14List, SerialDevicePlugin.settings.Button14);
+            SelectDropDownList(Btn15List, SerialDevicePlugin.settings.Button15);
+            SelectDropDownList(Btn16List, SerialDevicePlugin.settings.Button16);
+            SelectDropDownList(Btn17List, SerialDevicePlugin.settings.Button17);
+            SelectDropDownList(Btn18List, SerialDevicePlugin.settings.Button18);
+            SelectDropDownList(Btn19List, SerialDevicePlugin.settings.Button19);
+            SelectDropDownList(Btn20List, SerialDevicePlugin.settings.Button20);
+        }
+
+        //ComboBoxへアイテムの追加
+        private void AddKey(string s)
+        {
+            Btn1List.Items.Add(s);
+            Btn2List.Items.Add(s);
+            Btn3List.Items.Add(s);
+            Btn4List.Items.Add(s);
+            Btn5List.Items.Add(s);
+            Btn6List.Items.Add(s);
+            Btn7List.Items.Add(s);
+            Btn8List.Items.Add(s);
+            Btn9List.Items.Add(s);
+            Btn10List.Items.Add(s);
+            Btn11List.Items.Add(s);
+            Btn12List.Items.Add(s);
+            Btn13List.Items.Add(s);
+            Btn14List.Items.Add(s);
+            Btn15List.Items.Add(s);
+            Btn16List.Items.Add(s);
+            Btn17List.Items.Add(s);
+            Btn18List.Items.Add(s);
+            Btn19List.Items.Add(s);
+            Btn20List.Items.Add(s);
+        }
+
+        //設定の呼び出し・各設定項目に反映
+        private void SelectDropDownList(ComboBox list, int[] assign)
+        {
+            switch(assign[0])
+            {
+                case -1:
+                    if (assign[1] >= 0 && assign[1] < cabSwitchKeyTexts.Length)
+                    {
+                        list.SelectedIndex = assign[1];
+                    }
+                    break;
+                case -2:
+                    if (assign[1] >= 0 && assign[1] < atsKeyTexts.Length)
+                    {
+                        list.SelectedIndex = assign[1] + cabSwitchKeyTexts.Length;
+                    }
+                    break;
             }
         }
 
@@ -107,7 +212,107 @@ namespace SerialDevicePlugin
         //"OK"がクリック
         private void Ok_Click(object sender, EventArgs e)
         {
+            if(ComPorts.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Port = ComPorts.SelectedIndex + 1;
+            }
 
+            if(ComSpeedList.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Speed = ComSpeedList.SelectedIndex + 1;
+            }
+
+            if(Btn1List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button1 = keyCodeTable[Btn1List.SelectedIndex];
+            }
+            if (Btn2List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button2 = keyCodeTable[Btn2List.SelectedIndex];
+            }
+            if (Btn3List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button3 = keyCodeTable[Btn3List.SelectedIndex];
+            }
+            if (Btn4List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button4 = keyCodeTable[Btn4List.SelectedIndex];
+            }
+            if (Btn5List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button5 = keyCodeTable[Btn5List.SelectedIndex];
+            }
+            if (Btn6List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button6 = keyCodeTable[Btn6List.SelectedIndex];
+            }
+            if (Btn7List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button7 = keyCodeTable[Btn7List.SelectedIndex];
+            }
+            if (Btn8List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button8 = keyCodeTable[Btn8List.SelectedIndex];
+            }
+            if (Btn9List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button9 = keyCodeTable[Btn9List.SelectedIndex];
+            }
+            if (Btn10List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button10 = keyCodeTable[Btn10List.SelectedIndex];
+            }
+            if (Btn11List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button11 = keyCodeTable[Btn11List.SelectedIndex];
+            }
+            if (Btn12List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button12 = keyCodeTable[Btn12List.SelectedIndex];
+            }
+            if (Btn13List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button13 = keyCodeTable[Btn13List.SelectedIndex];
+            }
+            if (Btn14List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button14 = keyCodeTable[Btn14List.SelectedIndex];
+            }
+            if (Btn15List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button15 = keyCodeTable[Btn15List.SelectedIndex];
+            }
+            if (Btn16List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button16 = keyCodeTable[Btn16List.SelectedIndex];
+            }
+            if (Btn17List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button17 = keyCodeTable[Btn17List.SelectedIndex];
+            }
+            if (Btn18List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button18 = keyCodeTable[Btn18List.SelectedIndex];
+            }
+            if (Btn19List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button19 = keyCodeTable[Btn19List.SelectedIndex];
+            }
+            if (Btn20List.SelectedIndex >= 0)
+            {
+                SerialDevicePlugin.settings.Button20 = keyCodeTable[Btn20List.SelectedIndex];
+            }
+
+            DialogResult = DialogResult.OK;
+
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Serial Device Plugin -ERROR-", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         //"Cancel"がクリック
--- SerialDevicePlugin/Settings.cs
+++ SerialDevicePlugin/Settings.cs
@@ -15,6 +15,28 @@ namespace SerialDevicePlugin
         private int port = 0; //COMポート番号
         private int speed = 9600; //シリアル通信速度
 
+        //各ボタンの設定
+        private int[] button1  = { -1, 0 };
+        private int[] button2  = { -1, 1 };
+        private int[] button3  = { -1, 2 };
+        private int[] button4  = { -1, 3 };
+        private int[] button5  = { -2, 0 };
+        private int[] button6  = { -2, 1 };
+        private int[] button7  = { -2, 2 };
+        private int[] button8  = { -2, 3 };
+        private int[] button9  = { -2, 4 };
+        private int[] button10 = { -2, 5 };
+        private int[] button11 = { -2, 6 };
+        private int[] button12 = { -2, 7 };
+        private int[] button13 = { -2, 8 };
+        private int[] button14 = { -2, 9 };
+        private int[] button15 = { -2, 10 };
+        private int[] button16 = { -2, 11 };
+        private int[] button17 = { -2, 12 };
+        private int[] button18 = { -2, 13 };
+        private int[] button19 = { -2, 14 };
+        private int[] button20 = { -2, 15 };
+
         //各種設定をXMLに保存
         public void SaveToXml()
         {
@@ -73,6 +95,126 @@ namespace SerialDevicePlugin
         {
             get { return speed; }
             set { speed = value; }
+        }
+
+        public int[] Button1
+        {
+            get { return button1; }
+            set { button1 = value; }
+        }
+
+        public int[] Button2
+        {
+            get { return button2; }
+            set { button2 = value; }
+        }
+
+        public int[] Button3
+        {
+            get { return button3; }
+            set { button3 = value; }
+        }
+
+        public int[] Button4
+        {
+            get { return button4; }
+            set { button4 = value; }
+        }
+
+        public int[] Button5
+        {
+            get { return button5; }
+            set { button5 = value; }
+        }
+
+        public int[] Button6
+        {
+            get { return button6; }
+            set { button6 = value; }
+        }
+
+        public int[] Button7
+        {
+            get { return button7; }
+            set { button7 = value; }
+        }
+
+        public int[] Button8
+        {
+            get { return button8; }
+            set { button8 = value; }
+        }
+
+        public int[] Button9
+        {
+            get { return button9; }
+            set { button9 = value; }
+        }
+
+        public int[] Button10
+        {
+            get { return button10; }
+            set { button10 = value; }
+        }
+
+        public int[] Button11
+        {
+            get { return button11; }
+            set { button11 = value; }
+        }
+
+        public int[] Button12
+        {
+            get { return button12; }
+            set { button12 = value; }
+        }
+
+        public int[] Button13
+        {
+            get { return button13; }
+            set { button13 = value; }
+        }
+
+        public int[] Button14
+        {
+            get { return button14; }
+            set { button14 = value; }
+        }
+
+        public int[] Button15
+        {
+            get { return button15; }
+            set { button15 = value; }
+        }
+
+        public int[] Button16
+        {
+            get { return button16; }
+            set { button16 = value; }
+        }
+
+        public int[] Button17
+        {
+            get { return button17; }
+            set { button17 = value; }
+        }
+
+        public int[] Button18
+        {
+            get { return button18; }
+            set { button18 = value; }
+        }
+
+        public int[] Button19
+        {
+            get { return button19; }
+            set { button19 = value; }
+        }
+
+        public int[] Button20
+        {
+            get { return button20; }
+            set { button20 = value; }
         }
     }
 }
